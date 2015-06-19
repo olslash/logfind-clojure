1ae3196c139b500a0846a23cf7afda9e82efb2e9
Mitch R <olordhux+github@gmail.com>
2015-06-19 01:31:46 -0700

    add -o flag to do OR searches

--- src/logfind/core.clj
+++ src/logfind/core.clj
@@ -31,21 +31,44 @@
 (defn- join-and
   "joins the args into a single regex that will match them all (boolean AND)"
   [& args]
-  ( re-pattern
-   (str ".*" (string/join ".*" args) ".*")))
+  (re-pattern
+    (str ".*" (string/join ".*" args) ".*")))
+
+(defn- join-or
+  "joins the args into a single regex that will match them all (boolean OR)"
+  [& args]
+  (re-pattern
+    (str (string/join "|" args))))
 
 (defn- in-file
   "search a filereader for a pattern and return the filename if there was a match"
   [re file]
   (boolean (re-find re (slurp file))))
 
+(defn- flags
+  "get -flags from an input string"
+  [inp]
+  ; filter twice, one with a -* check and one for the rest
+  (filterv #(= (first %) \-) inp))
+
+(defn- arguments
+  "get args (no -) from an input string"
+  [inp]
+  ; filter twice, one with a -* check and one for the rest
+  (filterv #(not= (first %) \-) inp))
+
 (defn -main
   "search all files matching the patterns specified in ~/.logfind for the
-  strings given in args. Prints the filenames that contain matches."
+  strings given in args. Prints the filenames that contain matches.
+  uses arg1 AND arg2 unless -o flag set, then uses arg1 OR arg2"
   ([] (print "provide one or more string params to search"))
   ([& args]
-   (let [filemask (re-pattern (string/join "|" (read-config)))
-         searchpattern (apply join-and args)]
+   (let [arg-flags (flags args)
+         filemask (re-pattern (string/join "|" (read-config)))
+         searchpattern (apply (if (some #{"-o"} arg-flags)
+                                join-or
+                                join-and)
+                              (arguments args))]
      (doseq [file (regex-file-seq filemask (files search-root))]
        (if (in-file searchpattern file)
          (println (str file)))))))
